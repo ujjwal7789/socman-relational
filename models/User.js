112103f8e521cc8783007ea75d855375d8301e60
Ujjawal Kumar <madara.esoteric.studious@gmail.com>
2025-08-23 16:37:04 +0530

    Added model User.js

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const {DataTypes} = require('sequelize');
+const {sequelize} = require('../config/database');
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true,
+        },
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    role: {
+        type: DataTypes.ENUM('admin', 'resident'),
+        allowNull: false,
+    },
+    apartment_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true, // admin might not have
+    },
+
+
+}, {
+    tableName: 'users',
+    timestamps: true, // Sequelize will manage createdAt and updatedAt
+});
+
+module.exports = User;
